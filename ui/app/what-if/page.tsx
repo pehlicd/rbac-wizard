1f9b924e3f1e01435a27af8c530dfe564342a514
pehlicd <65170388+pehlicd@users.noreply.github.com>
2024-05-26 20:08:22 +0200

    feat: add what-if page struct

---
+++ ui/app/what-if/page.tsx
@@ -0,0 +1,71 @@
+"use client";
+import Editor from '@monaco-editor/react';
+import { Card, CardBody, CardHeader } from "@nextui-org/card";
+import {Badge, Button } from "@nextui-org/react";
+import { useTheme } from 'next-themes';
+import { useState } from 'react';
+import axios from 'axios';
+import WhatIfGraph from '@/components/whatIfGraph';
+
+export default function WhatIfPage() {
+    const { theme } = useTheme();
+    const isDarkMode = theme === 'dark';
+    const editorTheme = isDarkMode ? 'vs-dark' : 'light';
+
+    const [yamlContent, setYamlContent] = useState('');
+    const [graphData, setGraphData] = useState<{ nodes: any[]; links: any[] } | null>(null);
+
+    const handleEditorChange = (value: string | undefined) => {
+        setYamlContent(value || '');
+    };
+
+    const handleGenerateClick = async () => {
+        try {
+            const response = await axios.post('/api/what-if', { yaml: yamlContent });
+            setGraphData(response.data);
+        } catch (error) {
+            console.error('Error generating graph:', error);
+        }
+    };
+
+    return (
+        <section style={{ position: 'relative', width: '100%', height: '100vh', display: 'flex', flexDirection: 'column' }}>
+            {/*<div style={{*/}
+            {/*    position: 'absolute', top: '10px', right: '10px', background: '#ff5733', color: 'white', padding: '5px 10px', borderRadius: '3px',*/}
+            {/*    fontWeight: 'bold', zIndex: 1000*/}
+            {/*}}>*/}
+            {/*    Beta*/}
+            {/*</div>*/}
+            <div style={{ display: 'flex', flexDirection: 'row', flexGrow: 1 }}>
+                <Card className="p-4" style={{ flex: 1, marginRight: '5px' }}>
+                    <Badge content="Beta" color="warning" size="md" className="absolute">
+                        <CardHeader>
+                                <h2>Editor</h2>
+                        </CardHeader>
+                    </Badge>
+                    <CardBody style={{ height: '100%', padding: 10 }}>
+                        <Editor
+                            height="100%"
+                            defaultLanguage="yaml"
+                            defaultValue={``}
+                            className="p-1 rounded-b-lg"
+                            theme={editorTheme}
+                            onChange={handleEditorChange}
+                        />
+                    </CardBody>
+                </Card>
+                <Card className="p-4" style={{ flex: 1, marginLeft: '5px' }}>
+                    <CardHeader>
+                        <h2>Graph</h2>
+                    </CardHeader>
+                    <CardBody style={{ height: '100%', padding: 10 }}>
+                        {graphData && graphData.nodes && graphData.links && (
+                            <WhatIfGraph data={graphData} />
+                        )}
+                    </CardBody>
+                </Card>
+            </div>
+            <Button onClick={handleGenerateClick} style={{ marginTop: '10px', marginBottom: '10px', width: '100%' }}>Generate</Button>
+        </section>
+    );
+}
