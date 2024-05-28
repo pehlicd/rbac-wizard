446c83d3c7b1573a8a2bf0143dc8f0446ace82b8
pehlicd <65170388+pehlicd@users.noreply.github.com>
2024-05-28 23:51:36 +0200

    chore: update homebrew formula

--- rbac-wizard.rb
+++ rbac-wizard.rb
@@ -2,8 +2,8 @@ class RbacWizard < Formula
   desc "RBAC Wizard is a tool that helps you visualize and analyze the RBAC configurations of your Kubernetes cluster. It provides a graphical representation of the Kubernetes RBAC objects."
   homepage "https://github.com/pehlicd/rbac-wizard"
   url "https://github.com/pehlicd/rbac-wizard.git",
-      tag:      "v0.0.2",
-      revision: "81a9e14f58f6af312dbd83d55a88091d1a358ed9"
+      tag:      "v0.0.3",
+      revision: "9b4041cd7d83778ffbda8eb6f5314e5b0f449f9d"
   license "MIT"
   head "https://github.com/pehlicd/rbac-wizard.git", branch: "main"
 
